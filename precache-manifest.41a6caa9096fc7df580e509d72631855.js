86b79e75b8900e874a41a1281106dc59ef3c2e16
aabidkhan0786 <62328527+aabidkhan0786@users.noreply.github.com>
2020-09-14 20:15:50 +0530

    Updates

---
+++ precache-manifest.41a6caa9096fc7df580e509d72631855.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8f566054574f26abceac18de4f09e0",
+    "url": "/React_Gallery/index.html"
+  },
+  {
+    "revision": "9d84218b9ca6ce52e076",
+    "url": "/React_Gallery/static/css/2.b565b473.chunk.css"
+  },
+  {
+    "revision": "3d557c9f10e7b63d6dd7",
+    "url": "/React_Gallery/static/css/main.50f8da16.chunk.css"
+  },
+  {
+    "revision": "9d84218b9ca6ce52e076",
+    "url": "/React_Gallery/static/js/2.0d9538ef.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/React_Gallery/static/js/2.0d9538ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d557c9f10e7b63d6dd7",
+    "url": "/React_Gallery/static/js/main.8e839663.chunk.js"
+  },
+  {
+    "revision": "270b0924ca39a1c9b0e5",
+    "url": "/React_Gallery/static/js/runtime-main.08367452.js"
+  }
+]);
